8c41d75a1de67487fb568062caad7187889cd6da
Chris Woodford <chris.woodford@gmail.com>
2012-11-07 21:13:59 -0500

    added support for installation via composer

--- library/Zeus/Rest/Route/Chain.php
+++ library/Zeus/Rest/Route/Chain.php
@@ -5,8 +5,10 @@ class Zeus_Rest_Route_Chain extends Zend_Controller_Router_Route_Chain
 
     public static function getInstance(Zend_Config $config)
     {
+
         $defs = ($config->defaults instanceof Zend_Config) ? $config->defaults->toArray() : array();
         return new self($config->route, $defs);
+
     }
 
 }
